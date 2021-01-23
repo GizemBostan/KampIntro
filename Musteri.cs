a70487bf82cc59c623a0fdb4bdbabf0e5b4cf7ad
GizemBostan <32106230+GizemBostan@users.noreply.github.com>
2021-01-23 16:21:13 +0300

    Add files via upload

---
+++ Musteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDemoMetot
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriAdi { get; set; }
+        public int MusteriYasi { get; set; }
+        public string MusteriOzellik { get; set; }
+
+    }
+}
---
+++ MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDemoMetot
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "  " + musteri.MusteriAdi + "  " + musteri.MusteriYasi + "  " + musteri.MusteriOzellik);
+            Console.WriteLine("müşteriler eklenmiştir");
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "  " + musteri.MusteriAdi + "  " + musteri.MusteriYasi + "  " + musteri.MusteriOzellik );
+            Console.WriteLine("müşteriler silinmiştir");
+        }
+
+
+       
+        public void List(Musteri[] musteri)
+        {
+            for (int i = 0; i < musteri.Length; i++)
+            {
+                Console.WriteLine(musteri[i].Id + " "+ musteri[i].MusteriAdi +" " + musteri[i].MusteriYasi + " " + musteri[i].MusteriOzellik);
+   
+            }
+            Console.WriteLine("müşteriler listelendi");
+
+            
+        }
+    }
+}
