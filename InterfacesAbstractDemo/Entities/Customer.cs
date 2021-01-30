48ee2b6e16ee970434d0c87484d13145776a7869
gizem bostan <Q313914gizem@outlook.com>
2021-01-30 14:04:30 +0300

    30Ocak

---
+++ InterfacesAbstractDemo/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InterfacesAbstractDemo
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string NationaltyId { get; set; }
+
+    }
+}
